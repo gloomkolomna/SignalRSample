857b75dcf35a3ad3e4441671b106459a1483227e
Eugeny Belovolov <gloomkolomna@gmail.com>
2020-09-15 18:25:29 +0300

    Simple test

---
+++ SignalRSample.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRSample.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder().WithUrl("https://localhost:5001/sample").Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            var messages = new List<string>();
+            connection.On<string>("send", (message) => Console.WriteLine(message));
+
+            await connection.StartAsync();
+
+            while(true)
+            {
+                var msg = Console.ReadLine();
+                if (string.IsNullOrEmpty(msg))
+                    break;
+                await connection.InvokeAsync("SendMessage", msg);
+            }
+        }
+    }
+}
---
+++ SignalRSample.Server/HubConfigs/SendHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRSample.Server.HubConfigs
+{
+    public class SendHub : Hub
+    {
+        public async Task SendMessage(string sendMsg)
+        {
+            await Clients.All.SendAsync("send", sendMsg);
+        }
+    }
+}
